d20dc5780eaa79606ad095388c309d87af520e76
hd1801 <hardikdhawan0108@gmail.com>
2021-09-07 12:33:48 +0530

    update

--- Covid-19-TrackerAPP/app/src/main/java/com/example/covid_19_tracker/MainActivity.kt
+++ Covid-19-TrackerAPP/app/src/main/java/com/example/covid_19_tracker/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.covid_19_tracker
 
 import android.os.Bundle
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
@@ -16,16 +15,15 @@ class MainActivity : AppCompatActivity() {
         getData()
     }
     private fun getData(){
-        val textView :TextView = findViewById(R.id.text_input1)
+
         val queue = Volley.newRequestQueue(this)
         val url = "https://data.covid19india.org/v4/min/data.min.json"
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
             { response ->
-                textView.text= response.toString()
                 Toast.makeText(this,"api call success",Toast.LENGTH_LONG).show()
              },
             { error ->
-                // TODO: Handle error
+
                 Toast.makeText(this,"api call failure",Toast.LENGTH_LONG).show()
             }
         )
